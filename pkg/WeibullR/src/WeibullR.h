ccca9234e6c7df6d11bc20ae715afe7c9156db6e
jtormerod <jtormerod@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-01-30 02:11:23 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/weibull-r@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/WeibullR/src/WeibullR.h
+++ pkg/WeibullR/src/WeibullR.h
@@ -13,22 +13,15 @@ struct AbPval{
 // abremDebias code
 RcppExport SEXP MLEloglike(SEXP arg1, SEXP arg3, SEXP arg4, SEXP arg5, SEXP arg6);
 RcppExport SEXP MLEsimplex(SEXP arg1, SEXP arg2, SEXP arg3, SEXP arg4, SEXP arg5);
-// exported for testing purposes only. Never called from R.
+// used in secant method for determination of 3rd parameter optimization
 RcppExport SEXP MLEdMaxLLdx(SEXP arg1, SEXP arg2, SEXP arg3, SEXP arg4);
 //******************//
 // end abremDebias
 
 
 // abremPivotals code
-//RcppExport SEXP LSLRw2pXonY (SEXP arg1, SEXP arg2);
-//RcppExport SEXP LSLRw2pYonX (SEXP arg1, SEXP arg2);
-//RcppExport SEXP LSLRln2pXonY (SEXP arg1, SEXP arg2);
-//RcppExport SEXP LSLRln2pYonX (SEXP arg1, SEXP arg2);
-//RcppExport SEXP LSLRg2pXonY (SEXP arg1, SEXP arg2);
-//RcppExport SEXP LSLRg2pYonX (SEXP arg1, SEXP arg2);
-
 RcppExport SEXP LSLR(SEXP arg1, SEXP arg2, SEXP arg3, SEXP arg4);
-RcppExport SEXP pivotalMC(SEXP arg1, SEXP arg2, SEXP arg3, SEXP arg4, SEXP arg5, SEXP arg6,SEXP arg7,SEXP arg8);	
+RcppExport SEXP pivotalMC(SEXP arg1, SEXP arg2, SEXP arg3, SEXP arg4, SEXP arg5, SEXP arg6,SEXP arg7,SEXP arg8);
 RcppExport SEXP CallgetCCC2(SEXP arg1, SEXP model);
 RcppExport SEXP CallgetPvalue(SEXP arg1, SEXP arg2, SEXP arg3);
 
@@ -36,8 +29,10 @@ extern "C" double getCCC2( int F, int model);
 extern "C" struct AbPval getPvalue(int F, double R2, int model);
 // end abremPivotals
 
+// contour code
+RcppExport SEXP getContour(SEXP arg1, SEXP arg2, SEXP arg3, SEXP arg4, SEXP arg5, SEXP arg6, SEXP arg7);
 
-RcppExport  void R_init_WeibullR(DllInfo* info);
+extern "C" void R_init_WeibullR(DllInfo* info);
 
 #endif
 #endif
